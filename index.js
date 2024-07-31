bcb631ae7e739744a9173743bf32757046569612
Vlad2IO <10906779+Vlad2IO@users.noreply.github.com>
2024-08-01 01:40:11 +0300

    commit

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 const TelegramBot = require('node-telegram-bot-api');
+const express = require('express');
+const bodyParser = require('body-parser');
 const cron = require('node-cron');
 
-// Загрузка переменных окружения
+// Создаем приложение Express
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Загружаем переменные окружения
 const token = process.env.TELEGRAM_BOT_TOKEN;
 const chatId = process.env.TELEGRAM_GROUP_CHAT_ID;
+const url = process.env.VERCEL_URL; // Установите это в URL вашего развертывания Vercel
 
-if (!token || !chatId) {
-    console.error('Отсутствуют необходимые переменные окружения: TELEGRAM_BOT_TOKEN и TELEGRAM_GROUP_CHAT_ID');
+// Проверяем наличие переменных окружения
+if (!token || !chatId || !url) {
+    console.error('Отсутствуют необходимые переменные окружения');
     process.exit(1);
 }
 
-const bot = new TelegramBot(token, { polling: true });
+// Настраиваем бота
+const bot = new TelegramBot(token);
+bot.setWebHook(`${url}/bot${token}`);
+
+// Парсим тело запроса в формате JSON
+app.use(bodyParser.json());
+
+// Обрабатываем запросы на вебхук
+app.post(`/bot${token}`, (req, res) => {
+  bot.processUpdate(req.body);
+  res.sendStatus(200);
+});
 
 // Функция для разрешения отправки сообщений
 function enableChat() {
@@ -30,4 +49,7 @@ function disableChat() {
 cron.schedule('0 9 * * 1-5', enableChat); // Разрешение отправки с 9:00 каждый будний день
 cron.schedule('0 18 * * 1-5', disableChat); // Запрет отправки в 18:00 каждый будний день
 
-console.log("Бот запущен. Управление чатом активировано.");
+// Запускаем сервер
+app.listen(port, () => {
+  console.log(`Сервер запущен на порту ${port}`);
+});
