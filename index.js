fb24804b1f14c83d567ada080d3d72b8b100f9fe
Vlad2IO <10906779+Vlad2IO@users.noreply.github.com>
2024-08-01 02:14:31 +0300

    commit

--- index.js
+++ index.js
@@ -1,47 +1,47 @@
-const TelegramBot = require('node-telegram-bot-api');
-const cron = require('node-cron');
 const express = require('express');
 const bodyParser = require('body-parser');
+const TelegramBot = require('node-telegram-bot-api');
+const cron = require('node-cron');
+
+// Установка часового пояса (замените на нужный)
+process.env.TZ = 'Europe/Moscow';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Загрузка переменных окружения
 const token = process.env.TELEGRAM_BOT_TOKEN;
 const chatId = process.env.TELEGRAM_GROUP_CHAT_ID;
 const url = process.env.VERCEL_URL;
 
-// Инициализация бота
 const bot = new TelegramBot(token, { polling: true });
 
-// Функция для разрешения отправки сообщений
 function enableChat() {
     console.log(`Разрешение чата: ${new Date().toLocaleString()}`);
-    bot.setChatPermissions(chatId, { can_send_messages: true })
-        .then(() => console.log("Чат разрешен для отправки сообщений."))
-        .catch((error) => console.error("Ошибка при разрешении чата:", error));
+    bot.setChatPermissions(chatId, { can_send_messages: true }).then(() => {
+        console.log("Чат разрешен для отправки сообщений.");
+    }).catch((error) => {
+        console.error("Ошибка при разрешении чата:", error);
+    });
 }
 
-// Функция для запрета отправки сообщений
 function disableChat() {
     console.log(`Запрет чата: ${new Date().toLocaleString()}`);
-    bot.setChatPermissions(chatId, { can_send_messages: false })
-        .then(() => console.log("Чат запрещен для отправки сообщений."))
-        .catch((error) => console.error("Ошибка при запрете чата:", error));
+    bot.setChatPermissions(chatId, { can_send_messages: false }).then(() => {
+        console.log("Чат запрещен для отправки сообщений.");
+    }).catch((error) => {
+        console.error("Ошибка при запрете чата:", error);
+    });
 }
 
-// Настройка cron-задач для управления чатом
-cron.schedule('0 9 * * 1-5', enableChat);  // Активация в 9:00 по будням
-cron.schedule('0 18 * * 1-5', disableChat);  // Деактивация в 18:00 по будням
+cron.schedule('0 9 * * 1-5', enableChat);
+cron.schedule('0 18 * * 1-5', disableChat);
 
-// Настройка сервера для приема запросов от Telegram
 app.use(bodyParser.json());
 app.post(`/bot${token}`, (req, res) => {
     bot.processUpdate(req.body);
     res.sendStatus(200);
 });
 
-// Вывод информации на главную страницу
 app.get('/', (req, res) => {
     res.send(`Telegram Schedule Bot активен и работает!<br>
               TOKEN: ${token}<br>
